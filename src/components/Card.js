43127cc14608363e792d85007ee120721469635c
Bomowo <oeccc25@yahoo.com>
2023-08-08 22:34:13 +0300

    fix styling for Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,7 +10,7 @@ export default function Card () {
                 <a href="https://goo.gl/maps/EP5J9aAFigoTGhnZ8">View on Google Maps</a>
             </header>
             <h1 className="place-name">Mount Fuji</h1>
-            <h2 className="date-of-visit">12 Jan, 2021 - 24 Jan, 2021</h2>
+            <p className="date-of-visit">12 Jan, 2021 - 24 Jan, 2021</p>
             <p className="place-desc">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
         </div>
     )

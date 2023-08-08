24bfd3871b6e5a13a18bfa3564c920553a110fdc
Bomowo <oeccc25@yahoo.com>
2023-08-08 20:55:34 +0300

    Add header component

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import logo from "../resource/Fill 213.svg"
+
+export default function Header () {
+    return (
+        <nav>
+            <img src={logo}></img>
+            <span>my travel journal.</span>
+        </nav>
+    )
+}

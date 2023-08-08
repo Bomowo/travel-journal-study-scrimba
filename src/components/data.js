9d9a537ea3eefa74b0de4957199f9b7f057ae860
Bomowo <oeccc25@yahoo.com>
2023-08-08 22:46:26 +0300

    Fill out the data with our set

--- src/components/data.js
+++ src/components/data.js
@@ -1,4 +1,4 @@
-export default [
+const data = [
     {
         id: 1,
         photo: 'https://source.unsplash.com/WLxQvbMyfas',
@@ -10,20 +10,22 @@ export default [
     },
     {
         id: 2,
-        photo: 'https://source.unsplash.com/WLxQvbMyfas',
-        country: 'JAPAN',
-        maps: "https://goo.gl/maps/EP5J9aAFigoTGhnZ8",
-        name: 'Mount Fuji',
-        date: '12 Jan, 2021 - 24 Jan, 2021',
-        desc: 'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.'
+        photo: 'https://source.unsplash.com/JmuyB_LibRo',
+        country: 'AUSTRALIA',
+        maps: "https://goo.gl/maps/JFpa7jVudLEpu9aw9",
+        name: 'Sydney Opera House',
+        date: '27 May, 2021 - 8 Jun, 2021',
+        desc: 'The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century\'s most famous and distinctive buildings'
     },
     {
         id: 3,
-        photo: 'https://source.unsplash.com/WLxQvbMyfas',
-        country: 'JAPAN',
-        maps: "https://goo.gl/maps/EP5J9aAFigoTGhnZ8",
-        name: 'Mount Fuji',
-        date: '12 Jan, 2021 - 24 Jan, 2021',
-        desc: 'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.'
+        photo: 'https://source.unsplash.com/3PeSjpLVtLg',
+        country: 'NORWAY',
+        maps: "https://goo.gl/maps/L1XZtvF8Kozm98Lp6",
+        name: 'Geirangerfjord ',
+        date: '01 Oct, 2021 - 18 Nov, 2021',
+        desc: 'The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.'
     }
 ]
+
+export default data
